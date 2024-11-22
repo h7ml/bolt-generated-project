464945902b773aea51fd43a99ffabe0b943cf13e
h7ml <h7ml@qq.com>
2024-11-22 10:35:24 +0800

    Initial commit from your app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import TodoList from './components/TodoList';
 import BingWallpaper from './components/BingWallpaper';
@@ -7,12 +7,14 @@ import 'antd/dist/reset.css';
 
 function App() {
   return (
-    <BingWallpaper>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/todos" element={<TodoList />} />
-      </Routes>
-    </BingWallpaper>
+    <Router basename="/bolt-generated-project">
+      <BingWallpaper>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/todos" element={<TodoList />} />
+        </Routes>
+      </BingWallpaper>
+    </Router>
   );
 }
 
